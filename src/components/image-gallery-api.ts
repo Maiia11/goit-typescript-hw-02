ef2a7058c93377fc43edb76427b8f0ad3ec357dd
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-07-09 19:42:23 +0300

    add types in API

---
+++ src/components/image-gallery-api.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const acceptKey = "zDei0CTQ82bIpGgBAWl-KIKshW8lXqg3k_pY_IM5bRY"
+
+axios.defaults.baseURL = `https://api.unsplash.com`
+
+interface Image {
+    id: number,
+    urls: {
+        small: string,
+        regular: string,
+    },
+    alt_description: string,
+    likes: number,
+    user: {
+        name: string
+    }
+}
+
+interface Response{
+    total: number;
+  total_pages: number;
+  results: Image[];
+}
+
+export const getImageGallery = async (searchQuery:string, page:number): Promise<Response> => {
+    const { data } = await axios.get<Response>(`/search/photos?client_id=${acceptKey}`, {
+        params: {
+            query: searchQuery,
+            per_page: 9,
+            page
+        }
+    })
+    return data
+}
